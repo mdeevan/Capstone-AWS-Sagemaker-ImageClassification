922aab56dac2b91d873c1401271153ed8c0fae73
Muhammad Naveed <mnaveed@outlook.com>
2025-01-06 17:29:37 +0000

    hyperparameter tuning completed

--- train_serve_model.py
+++ train_serve_model.py
@@ -13,16 +13,13 @@ import argparse
 import logging
 import sys
 import os
+import smdebug
 
-# from model import net
+import smdebug.pytorch as smd
 
-# import smdebug
-
-# import smdebug.Pytorch as smd
-
-# from smdebug import modes
-# from smdebug.profiler.utils import str2bool
-# from smdebug.pytorch import get_hook
+from smdebug import modes
+from smdebug.profiler.utils import str2bool
+from smdebug.pytorch import get_hook
 
 
 
@@ -35,14 +32,19 @@ from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
-def test(model, test_dataloader, criterion, device=torch.device("cpu")):
+
+def test(model, test_dataloader, criterion, hook, device=torch.device("cpu")):
     '''
     TODO: Complete this function that can take a model and a 
           testing data loader and will get the test accuray/loss of the model
           Remember to include any debugging/profiling hooks that you might need
     '''
 
-    logger.info('HPO: model test starting')
+    logger.info('model training: model test starting')
+
+    if hook:
+        hook.set_mode(smd.modes.TRAIN) # assign the debugger hook
+
 
     model.eval()
     with torch.no_grad():
@@ -66,35 +68,41 @@ def test(model, test_dataloader, criterion, device=torch.device("cpu")):
         test_loss = test_running_loss / len(test_dataloader)
         accuracy  = accuracy_running  / len(test_dataloader)
 
-        logger.info("HPO: Test loss : {:.3f},\
+        logger.info("model training: Test loss : {:.3f},\
             Test set: Accuracy: {:.3f} ".format(test_loss,
                                         accuracy
                                         ))
 
 
-        logger.info("HPO: model testing completed")
+        logger.info("model training: model testing completed")
 
-def train(model, train_dataloader, valid_dataloader, criterion, optimizer, epochs=2, device=torch.device("cpu")):
+def train(model, train_dataloader, valid_dataloader, criterion, optimizer, hook, epochs=2, device=torch.device("cpu")):
     '''
     TODO: Complete this function that can take a model and
           data loaders for training and will get train the model
           Remember to include any debugging/profiling hooks that you might need
     '''
 
+    # hook = get_hook(create_if_not_exists=True)
+
     train_loss = []
     valid_loss = []
     accuracy   = []
     prev_accuracy = 0
 
-    logger.info('HPO: Training started on device {}'.format(device))
+    logger.info('model training: Training started on device {}'.format(device))
 
-  
     for epoch in range(epochs):
 
         train_running_loss = 0
         valid_running_loss = 0
         accuracy_running   = 0
         
+
+
+        if hook:
+            hook.set_mode(smd.modes.TRAIN) # assign the debugger hook
+
         model.train()
         for data, target in train_dataloader:
             data   = data.to(device)
@@ -108,6 +116,11 @@ def train(model, train_dataloader, valid_dataloader, criterion, optimizer, epoch
         
             loss.backward()
             optimizer.step()
+
+
+
+        if hook:
+            hook.set_mode(smd.modes.EVAL) # assign the debugger hook
 
         model.eval()
         with torch.no_grad():
@@ -138,7 +151,7 @@ def train(model, train_dataloader, valid_dataloader, criterion, optimizer, epoch
         valid_loss.append(valid_running_loss)
         accuracy.append(accuracy_running)
 
-        logger.info("HPO: epoch {} of {},\
+        logger.info("model training: epoch {} of {},\
             training loss : {:.3f},\
             validation loss : {:.3f},\
             accuracy : {:.3f} ".format(epoch+1, epochs,
@@ -148,17 +161,21 @@ def train(model, train_dataloader, valid_dataloader, criterion, optimizer, epoch
                                             ))
 
 
-    return model    
+    return model   #, train_loss, valid_loss, accuracy
 
 def net(num_classes):
     '''
     TODO: Complete this function that initializes your model
           Remember to use a pretrained model
     '''
-    model = models.resnet50(pretrained=True)
-    # model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-    # model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
 
+    # model = torchvision.models.detection.ResNet50_Weights()
+    # model = get_pretained_model_ResNet50()
+
+
+    # model = models.resnet50(pretrained=True)
+    model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+    model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
     # model = models.resnet18(pretrained=True)
     
     for params in model.parameters():
@@ -166,27 +183,25 @@ def net(num_classes):
 
     num_features = model.fc.in_features
 
+    model.fc = nn.Sequential(
+                    nn.Linear(num_features, 512),
+                    nn.ReLU(),
+                    nn.Linear(512         , num_classes),
+                    nn.Softmax(dim=1)
+                    )
     # model.fc = nn.Sequential(
     #                 nn.Linear(num_features, 1024),
     #                 nn.ReLU(),
     #                 nn.Linear(1024        , 512),
     #                 nn.ReLU(),
-    #                 nn.Linear(512         , num_classes),
-    #                 nn.Softmax(dim=1)
-    #                 )
-    # model.fc = nn.Sequential(
-    #                 nn.Linear(num_features, 512),
+    #                 nn.Linear(512         , 256),
     #                 nn.ReLU(),
-    #                 nn.Linear(512         , num_classes),
+    #                 nn.Linear(256         , num_classes),
     #                 nn.Softmax(dim=1)
     #                 )
-    model.fc = nn.Sequential(
-                    nn.Linear(num_features , num_classes),
-                    nn.Softmax(dim=1)
-                    )
 
 
-    logger.info("HPO: Model creation completed")
+    logger.info("model training: Model creation completed")
     return model
 
 def create_data_loaders(data_train, data_valid, data_test, batch_size):
@@ -195,7 +210,7 @@ def create_data_loaders(data_train, data_valid, data_test, batch_size):
     depending on whether you need to use data loaders or not
     '''
 
-    logger.info("HPO: creating data loaders")
+    logger.info("model training: creating data loaders")
     mean = [0.485, 0.456, 0.406]
     std  = [0.229, 0.224, 0.225]
 
@@ -215,35 +230,38 @@ def create_data_loaders(data_train, data_valid, data_test, batch_size):
     validset = torchvision.datasets.ImageFolder(data_valid, transform=testing_transform)
     testset  = torchvision.datasets.ImageFolder(data_test , transform=testing_transform)
 
-    logger.info("HPO: Batch Size {}".format( batch_size))
+    logger.info("model training: Batch Size {}".format( batch_size))
     
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
     valid_loader = torch.utils.data.DataLoader(validset, batch_size=batch_size, shuffle=True)
     test_loader  = torch.utils.data.DataLoader(testset , batch_size=batch_size, shuffle=True)
 
-    logger.info('HPO: Data loaders created')
+    logger.info('model training: Data loaders created')
     return train_loader, valid_loader, test_loader
 
 
+    # pass
+
 def main(args):
-
-
     logger.info("arguments {}".format(args))
-    logger.info(f'HPO: Hyperparameters are LR: {args.lr}, Batch Size: {args.batch_size}')
+    logger.info(f'model training: Hyperparameters are LR: {args.lr}, Batch Size: {args.batch_size}')
 
     '''
     TODO: Initialize a model by calling the net function
     '''
 
     device = torch.device("cuda" if (torch.cuda.is_available() & args.gpu) else "cpu")
-    logger.info(f"HPO: Running on Device {device}")
+    logger.info(f"model training: Running on Device {device}")
 
-    logger.info(f"HPO: Retrieving model")
+    logger.info(f"model training: Retrieving model")
     model=net(args.num_classes)
     model.to(device)
 
+    logger.info(f"model training: setting up hook")
+    hook = smd.Hook.create_from_json_file()
+    hook.register_hook(model) 
 
-    logger.info('HPO: create the data loaders')
+    logger.info('model training: create the data loaders')
     train_loader, valid_loader, test_loader=create_data_loaders(args.data_train,  args.data_valid,args.data_test, args.batch_size )
 
 
@@ -255,28 +273,34 @@ def main(args):
 
     optimizer = optim.Adam(model.fc.parameters(), lr=args.lr) #using adam optimizer
 
+
+    if hook:
+        logger.info('model training: hook loss criterion')
+        hook.register_loss(loss_criterion)
+
     '''
     TODO: Call the train function to start training your model
     Remember that you will need to set up a way to get training data from S3
     '''
-    logger.info('HPO: train the model')
-    model=    train(model, train_loader, valid_loader, loss_criterion, optimizer, args.epochs, device)
+
+    logger.info('model training: train the model')
+    model=    train(model, train_loader, valid_loader, loss_criterion, optimizer, hook, args.epochs, device)
 
 
     '''
     TODO: Test the model to see its accuracy
     '''
-    logger.info("HPO: Test the Model")
-    test(model, test_loader, loss_criterion, device)
+    logger.info("model training: Test the Model")
+    test(model, test_loader, loss_criterion, hook, device)
     
+
     '''
     TODO: Save the trained model
     '''
-    logger.info("HPO: Saving Model")
+    logger.info("model training: Saving Model")
     torch.save(model.state_dict(), os.path.join(args.model_dir, "model.pth")) # save the trained model to S3
 
-    logger.info("HPO: Script completed, exiting")
-
+    logger.info("model training: Script competed, exiting")
 
 
 if __name__=='__main__':
@@ -286,22 +310,26 @@ if __name__=='__main__':
     '''
 #   https://github.com/aws/sagemaker-training-toolkit/blob/master/ENVIRONMENT_VARIABLES.md
 
+
     parser.add_argument('--data-train', type=str,      default=os.environ['SM_CHANNEL_TRAIN'])
     parser.add_argument('--data-test',  type=str,      default=os.environ['SM_CHANNEL_TEST'])
     parser.add_argument('--data-valid', type=str,      default=os.environ['SM_CHANNEL_VAL'])
     parser.add_argument('--model-dir',  type=str,      default=os.environ['SM_MODEL_DIR'])
-    # parser.add_argument('--user_args',  type=str,      default=os.environ['SM_USER_ARGS'])
+
+    # parser.add_argument("--num_classes",type=int,      default=os.environ['NUM_CLASSES']) #default=10,   metavar="N", help="Number of classes for classification (default = 10)")
+    # parser.add_argument("--gpu",        type=str2bool, default=True, metavar="N", help="Train on GPU, (default = True)")
+
+    # parser.add_argument('--output-dir', type=str,      default=os.environ['SM_OUTPUT_DATA_DIR'])
+    # parser.add_argument("--data-path",  type=int,      default=os.environ['SM_CHANNEL_TRAINING'],   metavar="N", help="S3 location of train/test/valid data")
 
     parser.add_argument("--batch-size", type=int,      default=64,   metavar="N", help="input batch size for training (default: 64)",)
     parser.add_argument("--epochs",     type=int,      default=1,    metavar="N", help="number of epochs to train (default: 1)") 
     parser.add_argument("--lr",         type=float,    default=0.05, metavar="N", help="Learning rate (default = 0.05)")
-    parser.add_argument("--num-classes",type=int,      default=133,  metavar="N", help="Number of classes for classification (default = 133)")
+    parser.add_argument("--num-classes",type=int,      default=133,  metavar="N", help="Number of classes")
     parser.add_argument("--gpu",        type=bool,     default=True, metavar="N", help="gpu training, default = True")
 
-    args=parser.parse_args()
-    
 
-    logger.info("arguments = ", args)
-    print(f"arguments = {args}")
+
+    args=parser.parse_args()
     
     main(args)
